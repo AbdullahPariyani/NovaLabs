ade6c9cf43eb84b8254ecd9229be6cceb36e07da
Abdullah Mo Iliyas Pariyani <abdullahp@cybage.com>
2021-06-07 22:45:23 +0530

    design changes

--- assignment/mobile-app-buyer-react-native/Screen/DrawerScreens/FlatListDemo.js
+++ assignment/mobile-app-buyer-react-native/Screen/DrawerScreens/FlatListDemo.js
@@ -1,21 +1,25 @@
 
 
 // Import React and Component
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { ListItem, Icon, Button } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import { ListItem, Button } from 'react-native-elements';
+import Loader from '../Components/Loader';
 
 
 
 const FlatListDemo = () => {
 
     const [users, usersSet] = React.useState([]);
+    const [loading, setLoading] = useState(false);
 
     React.useEffect(() => {
         async function fetchUsers() {
+            setLoading(true);
             const fullResponse = await fetch('https://node-api-nova.herokuapp.com/seller/list-seller-with-slot');
             const responseJson = await fullResponse.json();
             usersSet(responseJson.data);
+            setLoading(false);
             console.log(fullResponse);
         }
 
@@ -23,34 +27,39 @@ const FlatListDemo = () => {
     }, []);
 
     return (
+        <SafeAreaView>
 
-        <View>
-            {
-                users.map((user, i) => (
-                    <ListItem key={user.seller._id} bottomDivider>
-                        {/* <Avatar source={{ uri: l.avatar_url }} /> */}
-                        <ListItem.Content bottomDivider>
-                            <ListItem.Title style={styles.title}>{user.seller.firstName} {user.seller.lastName}</ListItem.Title>
-                            <ListItem.Subtitle>{user.seller.email}</ListItem.Subtitle>
-                            {
-                                user.slots.map((slot, i) => (
-                                    <View style={{ flexDirection: 'row', marginTop: 10 }} key={slot._id}>
-                                        <View style={{ marginRight: 10 }} bottomDivider>
-                                            <Text>{slot.timeSlotValue}</Text>
-                                        </View>
-                                        <View bottomDivider>
-                                            <Button title={slot.timeSlotValue} onPress={() => { }}></Button>
-                                        </View>
-                                    </View>
-                                ))
-                            }
+            <ScrollView>
+                <View>
+                    <Loader loading={loading} />
+                    {
+                        users.map((user, i) => (
+                            <ListItem key={user.seller._id} bottomDivider>
+                                {/* <Avatar source={{ uri: l.avatar_url }} /> */}
+                                <ListItem.Content bottomDivider>
+                                    <ListItem.Title style={styles.title}>{user.seller.firstName} {user.seller.lastName}</ListItem.Title>
+                                    <ListItem.Subtitle>{user.seller.email}</ListItem.Subtitle>
+                                    {
+                                        user.slots.map((slot, i) => (
+                                            <View style={{ flexDirection: 'row', marginTop: 10 }} key={slot._id}>
+                                                <View style={{ marginRight: 10 }} bottomDivider>
+                                                    <Text>{slot.timeSlotValue}</Text>
+                                                </View>
+                                                <View bottomDivider>
+                                                    <Button title={slot.timeSlotValue} onPress={() => { }}></Button>
+                                                </View>
+                                            </View>
+                                        ))
+                                    }
 
-                        </ListItem.Content>
-                    </ListItem>
-                ))
-            }
+                                </ListItem.Content>
+                            </ListItem>
+                        ))
+                    }
 
-        </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
